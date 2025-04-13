c9382e1dd1fcef544326209cf957450b6f34288a
Souvik Ghosh <mesouvikghosh96@gmail.com>
2025-04-13 14:38:08 +0530

    first commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,18 @@
+import { BackgroundBeams } from '@/components/ui/background-beams'
+import React from 'react'
+
+function page() {
+  return (
+    <div className="h-[100vh] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-2xl mx-auto p-4">
+        <div className="flex items-center text-center justify-center">
+
+        </div>
+
+      </div>
+      <BackgroundBeams />
+    </div>
+  )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,69 @@
-import Image from "next/image";
+"use client"
+import { motion, Variants } from 'framer-motion'
+import { BackgroundBeams } from "../components/ui/background-beams";
+import { useTimer } from 'react-timer-hook'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  // Time counter
+  const eventDate = new Date('July 20, 2025 17:30:00')
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+  } = useTimer({ expiryTimestamp: eventDate })
+
+  const animation: Variants = {
+    hidden: { opacity: 0, y: 80 },
+    show: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: 'spring',
+        duration: 1.5,
+        staggerChildren: 0.1,
+      },
+    },
+  }
+  return (
+    <div className="h-[100vh] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-2xl mx-auto p-4">
+        <div className="flex items-center text-center justify-center">
+          <motion.div
+            variants={animation}
+            initial="hidden"
+            animate="show"
+            className="md:text-8xl font-medium text-6xl mt-9 z-50"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <motion.span variants={animation}>H</motion.span>
+            <motion.span variants={animation}>A</motion.span>
+            <motion.span variants={animation}>C</motion.span>
+            <motion.span variants={animation}>K</motion.span>
+            <motion.span variants={animation}>&#123;</motion.span>
+            <motion.span variants={animation} className="text-green-400">
+              0
+            </motion.span>
+            <motion.span variants={animation}>&#125;</motion.span>
+            <motion.span variants={animation}>L</motion.span>
+            <motion.span variants={animation}>U</motion.span>
+            <motion.span variants={animation}>T</motion.span>
+            <motion.span variants={animation}>I</motion.span>
+            <motion.span variants={animation}>O</motion.span>
+            <motion.span variants={animation}>N</motion.span>
+          </motion.div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className='flex justify-center items-center pt-2'>
+          <div className="text-3xl md:text-7xl z-50 relative text-green-200">
+            <span>{days}</span>.<span className="">{hours}</span>.
+            <span>{minutes}</span>.
+            <span className="">{seconds}</span>
+          </div>
+        </div>
+
+      </div>
+      <BackgroundBeams />
     </div>
   );
 }

fdc0344d36ecdbff33f1753fdfecdf33f4e235bd
Souvik Ghosh <mesouvikghosh96@gmail.com>
2025-06-24 12:11:29 +0530

    temporarily open submit button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -805,7 +805,7 @@ function Dashboard() {
 
                   {/* Project Submit button */}
 
-                  {/* <div className="flex justify-end mt-4">
+                  <div className="flex justify-end mt-4">
                     <button
                       className="px-4 py-2 bg-blue-600 hover:bg-blue-500 text-white rounded-md text-sm font-medium hover:cursor-pointer"
                       onClick={handleProjectSubmission}
@@ -820,7 +820,7 @@ function Dashboard() {
                         </>
                       )}
                     </button>
-                  </div> */}
+                  </div>
                 </div>
               )}
             </div>
